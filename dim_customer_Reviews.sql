5bd3439bc77485a12b8e49523d64d3814d0dc16a
Varun Raj <145306368+Thunder846@users.noreply.github.com>
2026-09-03 11:22:59 +0530

    Update dim_customer_Reviews.sql

--- dim_customer_Reviews.sql
+++ dim_customer_Reviews.sql
@@ -1,3 +1,4 @@
+Select * from dbo.customer_reviews; 
 -- This code selects review details from the customer_reviews table and uses REPLACE
 -- to remove extra spaces from the ReviewText, making the text cleaner and more consistent.
 SELECT 
