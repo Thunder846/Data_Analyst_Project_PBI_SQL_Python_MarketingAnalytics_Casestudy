373ea638fb88e3eadd60680a8e0fbe6e905836be
Varun Raj <145306368+Thunder846@users.noreply.github.com>
2026-09-06 03:44:52 +0530

    Create Importing_libraries.py

---
+++ Importing_libraries.py
@@ -0,0 +1,9 @@
+# Libraries used:
+# pandas      - Used for data manipulation and working with tables/DataFrames.
+# pyodbc      - Used to connect Python with SQL databases and execute SQL queries.
+# nltk        - Used for Natural Language Processing (NLP) tasks.
+# VADER       - Used to perform sentiment analysis on text (positive, negative, or neutral).
+import pandas as pd
+import pyodbc
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
